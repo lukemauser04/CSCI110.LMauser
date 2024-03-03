c499611d4c01bcfbd5b5bc450be3075807967403
lukemauser04 <157448447+lukemauser04@users.noreply.github.com>
2024-03-03 11:49:20 -0700

    Add files via upload

---
+++ 5.14.1 Week 5, week numbers.py
@@ -0,0 +1,29 @@
+d1 = "Sunday"
+d2 = "Monday"
+d3 = "Tuesday"
+d4 = "Wednesday"
+d5 = "Thursday"
+d6 = "Friday"
+d7 = "Saturday"
+
+x = int(input("Please enter a day number: "))
+
+if x == 1:
+    print(d1)
+elif x == 2:
+    print(d2)
+elif x == 3:
+    print(d3)
+elif x == 4:
+    print(d4)
+elif x == 5:
+    print(d5)
+elif x == 6:
+    print(d6)
+elif x == 7:
+    print(d7)
+        
+print("That is a great day!")
+
+print("Program Ended")
+
