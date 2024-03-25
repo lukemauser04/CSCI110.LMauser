3c0aa10b2149ab1d59be46dc0a13880b7f2e939e
lukemauser04 <157448447+lukemauser04@users.noreply.github.com>
2024-03-25 12:34:07 -0600

    Update Week 7 - 6.9.7.py
    Updated 3/25/24

--- Week 7 - 6.9.7.py
+++ Week 7 - 6.9.7.py
@@ -1,3 +1,5 @@
+import sys
+
 import math
 
 
@@ -7,3 +9,18 @@ def to_secs (hours, minutes, seconds):
     c = seconds
     result = a + b + c
     return result
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno  # This gets the callers line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+test(to_secs(2, 30, 11) == 9011)
+test(to_secs(2, 0, 0) == 7200)
+test(to_secs(0, 2, 0) == 120)
+test(to_secs(0, 0, 42) == 42)
+test(to_secs(0, -10, 10) == -590)
